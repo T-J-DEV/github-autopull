44205c652e5fb4d9a38fa8b07d2df81b9cdd087d
T-J-DEV <T-J-DEV@users.noreply.github.com>
2025-09-03 17:18:27 +1000

    518

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-# Only clone if repo is empty
 if [ ! -f /app/repo/index.html ]; then
   echo "Cloning repo..."
   git clone -b main https://github.com/T-J-DEV/github-autopull.git /app/repo
 fi
 
-# Auto-pull loop
 while true; do
   echo ">>> Fetching latest code..."
   cd /app/repo
