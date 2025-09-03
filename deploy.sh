c763996976d9b6bf3efd6124a414eb9106389d1a
T-J-DEV <T-J-DEV@users.noreply.github.com>
2025-09-03 16:51:18 +1000

    451

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 
-# Clone the repo if it doesn't exist
+# Ensure the repo is cloned
 if [ ! -f /app/repo/index.html ]; then
   rm -rf /app/repo/*
   git clone https://github.com/T-J-DEV/github-autopull.git /app/repo
 fi
 
-# Start auto-pull loop
+# Auto-pull loop
 while true; do
   echo ">>> Fetching latest code..."
   cd /app/repo
-  git fetch origin main
-  git reset --hard origin/main
+  git fetch origin refs/heads/main
+  git reset --hard FETCH_HEAD
   echo ">>> Pull complete, sleeping 30s..."
   sleep 30
 done
-chmod +x deploy.sh
